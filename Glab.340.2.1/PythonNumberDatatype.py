290b277e927d6cd2e7cce14234b5a2a4e2d64b5c
Jason Forrester <jason4ester1@gmail.com>
2024-10-11 08:37:13 -0500

    PA 340.2.1

---
+++ Glab.340.2.1/PythonNumberDatatype.py
@@ -0,0 +1,47 @@
+# 1. Add two integers
+int1 = 5
+int2 = 10
+int_sum = int1 + int2
+print("Sum of integers:", int_sum)
+
+# 2. Add two floats
+float1 = 5.5
+float2 = 10.5
+float_sum = float1 + float2
+print("Sum of floats:", float_sum)
+
+# 3. Add an integer and a float
+int_var = 7
+float_var = 3.5
+mixed_sum = int_var + float_var
+print("Sum of integer and float:", mixed_sum)  # The result is a float
+
+# 4. Divide two integers
+int3 = 20
+int4 = 4
+int_division = int3 / int4
+print("Division of integers:", int_division)
+
+# 5. Divide two floats
+float3 = 20.0
+float4 = 4.0
+float_division = float3 / float4
+print("Division of floats:", float_division)
+
+# 6. Cafe products and sales calculation
+coffee_price = 2.50
+cappuccino_price = 3.00
+espresso_price = 1.75
+
+# Order calculation
+subtotal = (3 * coffee_price) + (4 * cappuccino_price) + (2 * espresso_price)
+SALES_TAX = 0.07
+totalSale = subtotal + (subtotal * SALES_TAX)
+print("Subtotal:", subtotal)
+print("Total Sale with tax:", totalSale)
+
+# 7. Calculate the area of a circle
+radius = 63
+pi = 3.14
+area_of_circle = pi * (radius ** 2)
+print("Area of the circle:", area_of_circle)
